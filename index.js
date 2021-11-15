a4494c139b7eab843519cbb98889d9055c2aa1dc
SMG-CodeNextGen <94151165+SMG-CodeNextGen@users.noreply.github.com>
2021-11-14 18:07:57 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+function videos() {
+    window.location="videos.html";
+}
+
+function writings() {
+    window.location="writings.html";
+}
+
+function home() {
+    window.location="index.html";
+}
+
+function contact() {
+    window.location="contact.html";
+}
---
+++ videos.js
@@ -0,0 +1,15 @@
+function videos() {
+    window.location="videos.html";
+}
+
+function writings() {
+    window.location="writings.html";
+}
+
+function home() {
+    window.location="index.html";
+}
+
+function contact() {
+    window.location="contact.html";
+}
